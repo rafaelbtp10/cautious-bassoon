b91602c632a46736f2c7fb95e4327918e65a2506
Rafael Trigoli <144027331+rafaelbtp10@users.noreply.github.com>
2023-09-04 10:35:12 -0300

    Update criarpastas.py
    Otimização para o usuário inserir o caminho do arquivo excel.

--- criarpastas.py
+++ criarpastas.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import os
 
-file_loc = 'C:/Users/rafael.cardoso/OneDrive - PONTELAND DISTRIBUICAO SA/Área de Trabalho/DOCS_AM/Nomes_AM.xlsx'
+file_loc = str(input(r'Insira o caminho do arquivo Excel:\n'))
 df = pd.read_excel(file_loc)
 nomes = df.values.T[0].tolist()
 
-caminho = str(input('Insira o caminho de destino das pastas: ')).strip().replace('\\', '/')
+caminho = str(input(r'Insira o caminho de destino das pastas:\n')).strip()
 
 for i in nomes:
-    # INSERIR CAMINHO DE DESTINO:
     path = os.path.join(f'{caminho}', i)
     os.mkdir(path)
