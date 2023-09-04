d405dcc24848d33f70f5beb636385e4756c26069
Rafael Trigoli <144027331+rafaelbtp10@users.noreply.github.com>
2023-09-04 11:08:32 -0300

    Update criarpastas.py
    Otimização de código.

--- criarpastas.py
+++ criarpastas.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import os
 
-file_loc = str(input(r'Insira o caminho do arquivo Excel:''\n'))
-df = pd.read_excel(file_loc)
+excel_name = str(input('Insira o nome do arquivo Excel (Com a extensão):\n'))
+file_loc = str(input('Insira o caminho do arquivo Excel:\n')).strip().replace('\\', '/')
+loc_full = file_loc + ('/'+excel_name)
+df = pd.read_excel(loc_full)
 nomes = df.values.T[0].tolist()
-caminho = str(input(r'Insira o caminho de destino das pastas:''\n')).strip()
+caminho = str(input('Insira o caminho de destino das pastas:\n')).strip().replace('\\', '/')
 
 for i in nomes:
     path = os.path.join(f'{caminho}', i)
