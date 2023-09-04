b72a59fbb122f2af0a43d7d3370f36beb9fa5bb9
Rafael Trigoli <144027331+rafaelbtp10@users.noreply.github.com>
2023-09-04 10:53:30 -0300

    Update criarpastas.py
    Limpeza de código

--- criarpastas.py
+++ criarpastas.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import os
 
-file_loc = str(input(r'Insira o caminho do arquivo Excel:\n'))
+file_loc = str(input(r'Insira o caminho do arquivo Excel:''\n'))
 df = pd.read_excel(file_loc)
 nomes = df.values.T[0].tolist()
-
-caminho = str(input(r'Insira o caminho de destino das pastas:\n')).strip()
+caminho = str(input(r'Insira o caminho de destino das pastas:''\n')).strip()
 
 for i in nomes:
     path = os.path.join(f'{caminho}', i)
